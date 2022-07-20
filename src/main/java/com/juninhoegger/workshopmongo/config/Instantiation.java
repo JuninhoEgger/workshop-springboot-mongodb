5915e99e2b6046a470abc5229cc1c4fb5e3a865e
Junior Egger <junior.egger@gmail.com>
2022-07-20 18:51:04 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/juninhoegger/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.juninhoegger.workshopmongo.config;
+
+import com.juninhoegger.workshopmongo.domain.User;
+import com.juninhoegger.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import static java.util.Arrays.asList;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //Deleta os dados do banco de teste
+        userRepository.deleteAll();
+
+        //Instancia novos usuários
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        //Salva os objetos acima instanciados
+        userRepository.saveAll(asList(maria, alex, bob));
+    }
+}
