dc120e4d763a8e7cff498b21b0f5e06e0fb44f4f
Junior Egger <junior.egger@gmail.com>
2022-07-21 21:46:36 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/juninhoegger/workshopmongo/config/Instantiation.java
+++ src/main/java/com/juninhoegger/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.juninhoegger.workshopmongo.config;
 
 import com.juninhoegger.workshopmongo.domain.Post;
 import com.juninhoegger.workshopmongo.domain.User;
+import com.juninhoegger.workshopmongo.dto.AuthorDTO;
 import com.juninhoegger.workshopmongo.repository.PostRepository;
 import com.juninhoegger.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,20 +30,17 @@ public class Instantiation implements CommandLineRunner {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(getTimeZone("GMT"));
 
-        //Deleta os dados do banco de teste
         userRepository.deleteAll();
         postRepository.deleteAll();
 
-        //Instancia novos usuários
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
-        //Salva os objetos acima instanciados
         userRepository.saveAll(asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(asList(post1, post2));
     }
 }
